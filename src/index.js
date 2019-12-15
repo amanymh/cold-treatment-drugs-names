13164deebd1118f9a9871b11754163fdc71eb864
amany <amany.m.harb@gmail.com>
2019-12-15 15:16:13 +0200

    coldTreatmentDrugsNames

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import uniqueRandomArray from 'unique-random-array';
-import starWarsNames from './starwars-names.json';
+import coldTreatmentDrugsNames from './cold-treatment-drugs-names.json';
 
-const getRandomItem = uniqueRandomArray(starWarsNames);
+const getRandomItem = uniqueRandomArray(coldTreatmentDrugsNames);
 
 module.exports = {
-  all: starWarsNames,
-  random: random
+	all: coldTreatmentDrugsNames,
+	random: random
 };
 
 function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    const randomItems = [];
-    for (let i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
-    }
-    return randomItems;
-  }
+	if (number === undefined) {
+		return getRandomItem();
+	} else {
+		const randomItems = [];
+		for (let i = 0; i < number; i++) {
+			randomItems.push(getRandomItem());
+		}
+		return randomItems;
+	}
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,35 +1,35 @@
-import {expect} from 'chai';
-import starWars from './index';
+import { expect } from 'chai';
+import coldTreatmentDrugsNames from './index';
 
-describe('starwars-names', function() {
-  describe('all', function() {
-    it('should be an array of strings', function() {
-      expect(starWars.all).to.satisfy(isArrayOfStrings);
+describe('cold-treatment-drugs-names', function() {
+	describe('all', function() {
+		it('should be an array of strings', function() {
+			expect(coldTreatmentDrugsNames.all).to.satisfy(isArrayOfStrings);
 
-      function isArrayOfStrings(array) {
-        return array.every(function(item) {
-          return typeof item === 'string';
-        });
-      }
-    });
+			function isArrayOfStrings(array) {
+				return array.every(function(item) {
+					return typeof item === 'string';
+				});
+			}
+		});
 
-    it('should contain `Luke Skywalker`', function() {
-      expect(starWars.all).to.include('Luke Skywalker');
-    });
-  });
+		it('should contain `panadol`', function() {
+			expect(coldTreatmentDrugsNames.all).to.include('panadol');
+		});
+	});
 
-  describe('random', function() {
-    it('should return a random item from the starWars.all', function() {
-      var randomItem = starWars.random();
-      expect(starWars.all).to.include(randomItem);
-    });
+	describe('random', function() {
+		it('should return a random item from the coldTreatmentDrugsNames.all', function() {
+			var randomItem = coldTreatmentDrugsNames.random();
+			expect(coldTreatmentDrugsNames.all).to.include(randomItem);
+		});
 
-    it('should return an array of random items if passed a number', function() {
-      var randomItems = starWars.random(3);
-      expect(randomItems).to.have.length(3);
-      randomItems.forEach(function(item) {
-        expect(starWars.all).to.include(item);
-      });
-    });
-  });
+		it('should return an array of random items if passed a number', function() {
+			var randomItems = coldTreatmentDrugsNames.random(3);
+			expect(randomItems).to.have.length(3);
+			randomItems.forEach(function(item) {
+				expect(coldTreatmentDrugsNames.all).to.include(item);
+			});
+		});
+	});
 });
